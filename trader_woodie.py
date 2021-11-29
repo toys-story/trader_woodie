78a63821b056f2f9c41c2c259d49465b962ff06b
CHULMING <ccm2819@naver.com>
2021-11-30 02:00:33 +0900

    work 1130

--- trader_woodie.py
+++ trader_woodie.py
@@ -1,11 +1,93 @@
-from toybox.toybox.base import baseTrader
+import os
+import sys
+sys.path.append(os.curdir)
+
+from toybox.src.toybox.base import baseTrader
+from toybox.src.toybox.libs import get_data, get_std
 
 
 class woodieTrader(baseTrader):
+    
+    def __init__(self, capital=0, test_mode=True):
+        super().__init__(capital=capital)
+        self.target_market = "KRW-SAND"
+        self.from_date = [2021, 11, 29, 0, 0, 0]
+        self.data = get_data(self.client, market=self.target_market,from_date=self.from_date)
+        
+        self.buy_ratio = 0.5
+        self.sell_ratio = 1.0
+        self.stds = get_std(data=self.data)
+        self.buy_conditions = [    
+            self.stds["Hclose_by_open"] * 100,
+            self.stds["Htail_by_close"] * 100,
+        ]
+        self.sell_conditions = [
+            self.stds["Lclose_by_open"] * 100,
+            self.stds["Ltail_by_close"] * 100,
+        ]
+    
+    def check_conditions(self, data) -> str:
+        open = float(data.get("opening_price", ""))
+        close = float(data.get("trade_price", ""))
+        high = float(data.get("high_price", ""))
+        low = float(data.get("low_price", ""))
+        time = data.get("candle_date_time_kst", "")
+        
+        positive_candle = True if close - open > 0 else False
+        open_to_close = abs((close - open) / open * 100)
+        decision = "Stay"
+        desc = ""
 
-    def __init__(self):
-        pass
+        if positive_candle:
+            close_to_high = (high - open) / open * 100 - open_to_close
+            desc = f"{time}, [Buy] Close : {round(open_to_close , 2)}, Close compared to High : {round(close_to_high, 2)}"
+        else:
+            close_to_low = (low - open) / open * (-100) - open_to_close
+            desc = f"{time}, [Sell] Close : {round(open_to_close , 2)}, Close compared to Low : {round(close_to_low, 2)}"
+
+        if positive_candle:
+            if open_to_close >= self.buy_conditions[0] and close_to_high <= self.buy_conditions[1]:
+                # Price is higher than std and upper-tail is lower than std
+                decision = "Buy"
+        else:
+            if open_to_close >= self.sell_conditions[0]:
+                # The Price is lower than std
+                decision = "Sell"
+        return decision, desc
+            
+        
 
 if __name__ == "__main__":
-    t = woodieTrader()
-    print(t)
+    trader = woodieTrader(capital=1000000)
+    
+    print(f"data : {len(trader.data)} EA")
+    print(f"buy_conditions : {trader.buy_conditions}")
+    print(f"sell_conditions : {trader.sell_conditions}")
+    
+    try:
+        for d in trader.data:
+            market = d.get("market", None)
+            price = d.get("trade_price", None)
+            time = d.get("candle_date_time_kst", None)
+            
+            if not market or not price or not time:
+                print(f"Error : {market}, {price}, {time}")
+                raise RuntimeError("Error : invaild data")
+
+            decision, desc = trader.check_conditions(d)
+            if decision == "Buy":
+                if trader.buy_stock(market=market, price=price, time=time, buy_ratio=trader.buy_ratio):
+                    print(desc)
+            elif decision == "Sell":
+                if trader.sell_stock(market=market, price=price, time=time, sell_ratio=trader.sell_ratio):
+                    print(desc)
+            else:
+                # Stay ~!
+                pass
+    except Exception as e:
+        print(e)
+    finally:
+        if trader.sell_stock(market=market, price=price, time=time, sell_ratio=trader.sell_ratio):
+            print("sell all of stocks")
+    trader.show_trade_history()
+    trader.account.show_account()
