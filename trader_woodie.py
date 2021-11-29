9d2e9dd1170641df9b1df78d7c4d9a93a3c793c8
ccm2819@naver.com <chulminchae@CHULMING.local>
2021-11-29 20:18:48 +0900

    setup

---
+++ trader_woodie.py
@@ -0,0 +1,11 @@
+from toybox.toybox.base import baseTrader
+
+
+class woodieTrader(baseTrader):
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    t = woodieTrader()
+    print(t)
